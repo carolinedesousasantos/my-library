f4250a30385700bf1845b45e07ac3897a1ff30cb
carolinedesousasantos <caroline.dsosa@gmail.com>
2023-07-05 10:51:54 -0300

    InitialCommit

---
+++ src/components/CommonTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-md">
+    <q-table title="Treats" :rows="rows" :columns="columns" row-key="name" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CommonTable",
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+    },
+
+    rows: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
